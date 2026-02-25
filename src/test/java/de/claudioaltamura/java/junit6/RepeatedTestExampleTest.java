7f4091af7610dffa47789cc6bfddb335bfc8248b
claudioaltamura <claudio@claudioaltamura.de>
2026-02-25 17:05:54 +0100

    fix: disabled test

--- src/test/java/de/claudioaltamura/java/junit6/RepeatedTestExampleTest.java
+++ src/test/java/de/claudioaltamura/java/junit6/RepeatedTestExampleTest.java
@@ -1,5 +1,6 @@
 package de.claudioaltamura.java.junit6;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.RepeatedTest;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -8,12 +9,13 @@ class RepeatedTestExampleTest {
 
     private final RepeatedTestExample repeatedTestExample = new RepeatedTestExample();
 
+    @Disabled("Disabled because execute() method gives intentionally random results.")
     @RepeatedTest(value = 10,
             name = "test {currentRepetition} of {totalRepetitions}",
             failureThreshold = 4)
     void repeatedTestWithExecute() {
         var ret = repeatedTestExample.execute();
-        System.out.println("isSuccessful() returned: " + ret);
+
         assertTrue(ret, "execute() should return true");
     }
 }
